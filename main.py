43f47faa76a5a203aa01fd245e0ce2327ba471c6
Cedrick Tan <168982674+cecece-c@users.noreply.github.com>
2024-06-02 00:27:18 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Display banner
+print("\nWelcome to the Band Name Generator.")
+
+
+# Get city name (string)
+city = input("\nWhat's the name of the city you grew up in?\n")
+
+
+# Get pet's name (string)
+pet = input("\nWhat's your pet's name?\n")
+
+
+# Display band name suggestion
+print(f"\nYour band name could be:\n{city.capitalize()} {pet.capitalize()}\n")
