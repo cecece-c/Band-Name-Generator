7ffd168bc528c89310943f6ee698d9c286b1c3a3
Cedrick Tan <168982674+cecece-c@users.noreply.github.com>
2024-06-02 14:04:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 print("\nWelcome to the Band Name Generator.")
 
 
-# Get city name and store it in variable 'city' (String)
+# Get city name and store it in 'city' (String)
 city = input("\nWhat's the name of the city you grew up in?\n")
 
 
-# Get pet's name and store it in variable 'pet' (String)
+# Get pet's name and store it in 'pet' (String)
 pet = input("\nWhat's your pet's name?\n")
 
 
