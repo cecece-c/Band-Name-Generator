526199fcf466ffe6cb92827eba78587b8e3076b4
Cedrick Tan <168982674+cecece-c@users.noreply.github.com>
2024-06-19 00:23:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,26 @@
 import time
 
 
-# Display banner
+# Display welcome message
 print("\nWelcome to the Band Name Generator.")
 
 
-# Get city name and store value in 'city' (String)
+# Get city name and store it in 'city' (String)
 city = input("\nWhat's the name of the city you grew up in?\n")
 
 
-# Get pet's name and store value in 'pet' (String)
+# Get pet's name and store it in 'pet' (String)
 pet = input("\nWhat's your pet's name?\n")
 
 
-# Display suggestion for band name
+# Display suggestion
 print(f"\nYour band name could be:\n{city.capitalize()} {pet.capitalize()}")
 
 
-# Exit program
+# Display exit message
 print("\nProgram exiting...")
+
+
+# Exit program after 5 second delay
 for delay in range(5):
     time.sleep(1)
